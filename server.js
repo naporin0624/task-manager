474cc8cbbfbc4e41da994827f0faf5f82e87b617
naporitan <>
2019-02-01 15:36:23 +0900

    update for heroku

--- server.js
+++ server.js
@@ -1,26 +1,10 @@
-// expressの呼び込み
-const express = require('express');
-// PORTの設定
-// もしenvにportが確立したらそのポートを、していなかったら8080を使う
-const port = process.env.PORT || 8080;
-// express()のインスタンス
-const app = express();
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-// どこにアクセスさせるかの設定
-// distフォルダ：コンパイルされたファイルが入る場所
-app.use(express.static(__dirname + "/dist/"));
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-// SPAの時、ルーティングがうまくいかない時があるので以下の設定
-// これをしていないと、https://~~~/aboutとかに行った時にリロードするとエラーがでる
-// SPAだとaboutファイルを直で読み込んでいないから
-// これでルート以外でリロードしてもindex.htmlを読み込んでちゃんとルーティングをしてくれる
-// /.*/で全てのルートを指定。req(request), res(response)
-app.get(/.*/, function(req, res) {
-    res.sendfile(__dirname + "/dist/index.html");
-});
-
-// 一番上で指定したportをlisten
-app.listen(port);
-
-// 動いてるかどうかconsoleで見る
-console.log("Server is up!!");
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
